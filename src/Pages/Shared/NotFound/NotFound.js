7424bd5f3b56a73c529e733626fa8eb249072e19
shakiv633 <shakibshuvo633@gmail.com>
2022-05-22 01:19:04 +0600

    extra ok

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -6,7 +6,7 @@ const NotFound= () => {
         <div>
             
             <p className="text-center"> <img  className="image"src="https://i.ibb.co/10GXxGy/404.jpg" alt="404" border="0"></img></p>
-         <p className="text-center fw-bold fs-4 text-info ">  Wrong Page </p>
+         <p className="text-center fw-bold fs-4 text-info ">Sorry you entire  Wrong Page </p>
         </div>
     );
 };
